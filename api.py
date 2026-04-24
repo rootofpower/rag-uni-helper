5fa40efc4e101eed06085e18c3ac34ac83b575e0
rootofpower <kurilyakoleksii12@gmail.com>
2026-04-24 17:39:27 +0200

    lint update

--- api.py
+++ api.py
@@ -4,7 +4,12 @@ from rag import get_answer_from_collection
 
 app = FastAPI()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Simple RAG helper"}
+
+
 @app.get("/ask")
 def ask(query: str):
     return get_answer_from_collection(query)
-
